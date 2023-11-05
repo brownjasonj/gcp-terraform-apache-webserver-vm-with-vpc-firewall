6bf4b0bf908365b2620633a2aa299cd8820530cb
Jason Brown <brownjasonj@gmail.com>
2023-11-05 22:15:38 +0100

    changed name of vm

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "google_compute_subnetwork" "default" {
 
 # Create a single Compute Engine instance
 resource "google_compute_instance" "default" {
-  name         = "apache-webserver-vm-modified"
+  name         = "apache-webserver-vm"
   machine_type = "f1-micro"
   zone         = local.zone
   tags         = ["ssh"]
